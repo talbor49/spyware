6ed807b48d33cf0b2f89af0b2b250f17eb22b1ca
talbor49 <talbor49@gmail.com>
2019-09-23 19:04:55 +0300

    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use std::thread;
 use std::io::{Error, Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::process::{Command, Output};
 use std::str::from_utf8;
+use std::thread;
 
 const CHUNKS: usize = 1024;
 const BIND_ADDR: &str = "0.0.0.0:1337";
 
 fn run_command(comm: &str) -> Result<Output, Error> {
-    Command::new(comm)
-        .output()
+    Command::new(comm).output()
 }
-
 
 fn handle_client(mut stream: TcpStream) {
     println!("Handling connection from {}", stream.peer_addr().unwrap());
@@ -25,9 +23,12 @@ fn handle_client(mut stream: TcpStream) {
             println!("Output: {}", from_utf8(&stdout).unwrap());
             stream.write(&stdout).unwrap();
             true
-        },
+        }
         Err(_) => {
-            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            println!(
+                "An error occurred, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
             stream.shutdown(Shutdown::Both).unwrap();
             false
         }
@@ -42,7 +43,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
+                thread::spawn(move || {
                     // connection succeeded
                     handle_client(stream)
                 });
